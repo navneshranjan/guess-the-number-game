cc4c609b3e100daa0f51fc3c9d9e8e25d5c9c84f
navneshranjan <navneshranjan@gmail.com>
2022-01-10 13:00:38 +0530

    high score

--- starter/script.js
+++ starter/script.js
@@ -9,6 +9,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,6 +26,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'correct number !!!!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     //when guess is too high
   } else if (guess > secretNumber) {
